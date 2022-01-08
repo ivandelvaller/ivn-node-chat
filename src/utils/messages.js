71cbb50c16a734becf23a6b0e1c50d4e4d54c4b8
Iván Del Valle <62220523+ivandelvaller@users.noreply.github.com>
2022-01-08 04:24:50 -0600

    format utils files

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -3,18 +3,18 @@ const generateMessages = (username, text) => {
         username,
         text,
         createdAt: new Date().getTime()
-    }
-}
+    };
+};
 
 const generateLocationMessage = (username, url) => {
     return {
         username,
         url,
         createdAt: new Date().getTime()
-    }
-}
+    };
+};
 
 module.exports = {
     generateMessages,
     generateLocationMessage
-}
+};
